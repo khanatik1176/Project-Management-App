16a32f37ed55d1d4ffa26e6d4d09e89fd31cf3cf
Khan Atik Faisal <khanatik1176@gmail.com>
2025-11-19 12:28:39 +0600

    database-config-added

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { config } from "./app.config";
+
+
+const connectDatabase = async () =>
+{
+    try {
+        await mongoose.connect(config.MONGO_URI);
+        console.log("Database connected successfully");
+        
+    } catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1);
+    }
+}
+
+export default connectDatabase;
