484df832de7aca386d7a65479131c82c47578be5
Khan Atik Faisal <khanatik1176@gmail.com>
2025-11-24 10:32:59 +0600

    frontend-added-bug-fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ import isAuthenticated from "./middlewares/isAuthenticate.middleware";
 import workSpaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.route";
 import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
